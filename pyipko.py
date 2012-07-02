75759d3eb9ef0f214d70dd0555cafa56eab2b6db
Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
2012-07-02 23:12:02 +0200

    Should work for simple file

--- pyipko.py
+++ pyipko.py
@@ -1,7 +1,9 @@
 import logging
 import datetime
+from decimal import Decimal
 
 logger = logging.getLogger(__name__)
+
 
 class Operation(object):
     def __init__(self):
@@ -11,15 +13,17 @@ class Operation(object):
         self.title = ''
         self.from_number = None
         self.from_addr = None
-        self.amount = {'curr':None, 'val':None}
-        self.ending_balance = {'curr':None, 'val':None}
+        self.amount = {'curr': None, 'val': None}
+        self.ending_balance = {'curr': None, 'val': None}
+
 
 class AccountHistory(object):
     def __init__(self):
         self.account = None
-        self.date = {'since':None, 'to':None}
+        self.date = {'since': None, 'to':None}
         self.filtering = None
         self.operations = list()
+
 
 class Converter(object):
     def parse_from_XML(self, filename):
@@ -94,42 +98,86 @@ class Converter(object):
                 logger.exception('Problem parsing operation data.')
 
     def to_mt940(self):
+        # Header
         mt940 = str()
-        mt940 += ':20:{0}\n\r'.format('MT940')
-        mt940 += ':25:{0}{1}\n\r'.format('PL', self.account_history.account)
-        mt940 += ':28C:{0}\n\r'.format('')
-        mt940 += ':60F:{0}{1}{2}{3}\n\r'.format('', '', '', '')
-        mt940 += ':61:{0} {1}\n\r'.format('', '')
+        mt940 += '\r\n'
+        mt940 += ':20:{0}\r\n'.format('MT940')
+        # Only polish accounts
+        mt940 += ':25:/{0}{1}\r\n'.format('PL', self.account_history.account)
+        mt940 += ':28C:{0:05}\r\n'.format(0)
+        # Sum the balance from begining
+        value_tmp = abs(Decimal(
+                self.account_history.operations[-1].amount['val']))
+        value_tmp += abs(Decimal(
+                self.account_history.operations[-1].ending_balance['val']))
+        # Format balance with comma, >0 value
+        value = '{0:.2f}'.format(value_tmp,).replace('.', ',')
+        ending_date = datetime.datetime.strptime(
+                self.account_history.date['since'], '%Y-%m-%d')
+        currency = self.account_history.operations[-1].ending_balance['curr']
+        mt940 += ':60F:{0}{1}{2}{3}\r\n'.format(
+                        'D' if value[0] == '-' else 'C',
+                        ending_date.strftime('%y%m%d'),
+                        currency.upper() if currency else '',
+                        value if value else '')
+        # Header end
         for operation in self.account_history.operations:
-            mt940 += ':86:{0}{1}\n\r'.format('020~00', '')
+            operation_str = str(operation.amount['val'])
+            # Operation description
+            mt940 += ':61:{0}{1}{2}{3}{4}{5}{6:010}\r\n'.format(
+                        operation.order_date.strftime('%y%m%d'),
+                        operation.exec_date.strftime('%m%d'),
+                        'D' if operation_str[0] == '-' else 'C',
+                        '{0:.2f}'.format(abs(Decimal(operation.amount['val']))).replace('.', ','),
+                        'S',
+                        '034',
+                        0)
+            # Only one type of operation
+            mt940 += ':86:{0}\r\n'.format('034',)
+            mt940 += ':86:{0}{1}\r\n'.format('034~00', '')
             logger.debug('operation = %s', operation)
             logger.debug('operation.title = %s', operation.title)
+            #Magic with description <=27 characters
             for line_num in range(6):
-                title_format = '~2{0}{1}\n\r'
+                title_format = '~2{0}{1}\r\n'
                 start = line_num * 27
                 line_len = 27
-                title_line = operation.title[start : start + line_len]
+                title_line = operation.title[start: start + line_len]
                 title_line = str(title_line) if title_line else chr(255)
                 mt940 += title_format.format(line_num, title_line)
-            mt940 += '~30{0}\n\r'.format('')
-            mt940 += '~31{0}\n\r'.format(operation.from_number)
+            mt940 += '~30{0}\r\n'.format(
+                        operation.from_number[3:12].replace(' ', ''),)
+            mt940 += '~31{0}\r\n'.format(
+                        operation.from_number[13:].replace(' ', ''),)
             addr = operation.from_addr or ''
-            mt940 += '~32{0}\n\r'.format(addr[0:27])
-            mt940 += '~33{0}\n\r'.format(addr[27:27+27] or chr(255))
-            mt940 += '~38{0}\n\r'.format('')
-            mt940 += '~60{0}\n\r'.format('')
-            mt940 += '~63{0}\n\r'.format('')
-        ending_date = datetime.datetime.strptime(
-                self.account_history.date['to'], '%Y-%m-%d')
-        currency = self.account_history.operations[-1].ending_balance['curr']
-        value = self.account_history.operations[-1].ending_balance['val']
-        mt940 += ':62F:{0}{1}{2}{3}\n\r'.format('D' if value[0] == '-' else 'C',
-                ending_date.strftime('%y%m%d'),
-                currency.upper() if currency else '',
-                value.replace('.', ',', 1)[1:] if value else '')
-        mt940 += ':64:{0}{1}{2}{3}\n\r'.format('D' if value[0] == '-' else 'C',
-                ending_date.strftime('%y%m%d'),
-                currency.upper() if currency else '',
-                value.replace('.', ',', 1)[1:] if value else '')
-        mt940 += '-\n\r'
+            mt940 += '~32{0}\r\n'.format(addr[0:27])
+            mt940 += '~33{0}\r\n'.format(addr[27:27 + 27] or chr(255))
+            mt940 += '~34{0}\r\n'.format('034')
+            mt940 += '~38{0}{1}\r\n'.format('PL',
+                        operation.from_number.replace(' ', ''))
+            # If empty -> (char)255
+            mt940 += '~63{0}\r\n'.format(chr(255))
+            # Balance here?
+            mt940 += '~64{0}\r\n'.format('')
+
+        # Footer
+        # TODO: Should be 'to' here?
+        footer_date = datetime.datetime.strptime(
+                self.account_history.date['since'], '%Y-%m-%d')
+
+        currency = self.account_history.operations[0].ending_balance['curr']
+        value = self.account_history.operations[0].ending_balance['val']
+        mt940 += ':62F:{0}{1}{2}{3}\r\n'.format(
+                    'D' if value[0] == '-' else 'C',
+                    footer_date.strftime('%y%m%d'),
+                    currency.upper() if currency else '',
+                    value.replace('.', ',', 1)[1:] if value else '')
+        mt940 += ':64:{0}{1}{2}{3}\r\n'.format(
+                    'D' if value[0] == '-' else 'C',
+                    footer_date.strftime('%y%m%d'),
+                    currency.upper() if currency else '',
+                    value.replace('.', ',', 1)[1:] if value else '')
+        mt940 += ':86:NAME ACCOUNT OWNER:\r\n'
+        mt940 += ':86:ACCOUNT DESCRIPTION:\r\n'
+        mt940 += '-\r\n'
         return mt940
